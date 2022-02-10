3bee86ec8ff238f9de72838662a7ed67c96005df
Reece Denne <rdenne@amazon.com>
2022-02-10 14:24:40 +1000

    Updated absockets appm to include Product (CFN template example)

--- absockets/absockets_stack.py
+++ absockets/absockets_stack.py
@@ -2,8 +2,17 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    #aws_s3 as s3, # Get S3
+    aws_servicecatalog_alpha as servicecatalog, # Get Service Catalog Construct from Construct Hub
+    aws_iam as iam
 )
+import aws_cdk
 from constructs import Construct
+
+# Resource Name
+import os
+current_user = os.getlogin()
+resourcename = current_user + "-absockets"
 
 class AbsocketsStack(Stack):
 
@@ -17,3 +26,21 @@ class AbsocketsStack(Stack):
         #     self, "AbsocketsQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        #
+        # bucket = s3.Bucket(self, resourcename, versioned=True,
+        #    removal_policy=aws_cdk.RemovalPolicy.DESTROY,
+        #    auto_delete_objects=True)
+        
+        portfolio =servicecatalog.Portfolio(self, "MyFirstPortfolio",
+            display_name="MyFirstPortfolio",
+            provider_name="MyTeam",
+            description="Portfolio for a project",
+            message_language=servicecatalog.MessageLanguage.EN
+        )
+
+        role = iam.Role(self, "Admin",
+            assumed_by=iam.AccountRootPrincipal()
+        )
+        portfolio.give_access_to_role(role)
+
